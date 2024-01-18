f493c7330a58dd0de21f3466c5fbcb029c9c1159
Aarush Inamdar <inamdara@uci.edu>
2024-01-18 00:02:26 -0800

    14

--- src/convert_knots.hpp
+++ src/convert_knots.hpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 double knots_to_miles_per_minute(int knot) {
-	double res =(knot*6076)/(double(5280)*double(60));
+	double res =(double(knot)*6076)/(double(5280)*double(60));
 	return res;
 }
 
