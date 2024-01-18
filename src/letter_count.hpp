99873a17d277b99d63da36d3ee448e81eade2d5a
Aarush Inamdar <inamdara@uci.edu>
2024-01-17 23:56:09 -0800

    12 HW1

--- src/letter_count.hpp
+++ src/letter_count.hpp
@@ -1,19 +1,35 @@
 int char_to_index(char ch)
 {
+if (ch >= 'A' && ch <= 'Z'){
+	return ch-'A';
 
+} else if (ch >='a' && ch <='z'){
+	return ch-'a';
+}
+return -1;
 }
 
 char index_to_char(int i)
 {
-
+return 'A'+i;
 }
 
-void count(string s, int counts[])
+void count(string str, int counts[])
+{
+for (char c : str){
+	int index = char_to_index(c);
+	if (index != -1){
+		counts[index]++;
+	}
+}
+}
+
+void print_counts(int counts[], int length)
 {
 
+for (int i = 0, i <length, i++) {
+	std::cout << index_to_char(i) << " " << counts[i] << '\n';
+	}
 }
 
-void print_counts(int counts[], int len)
-{
 
-}
