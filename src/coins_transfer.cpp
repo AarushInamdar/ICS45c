de5b8e5c5f155d34c3ccc79a483b827ed622e7d7
Aarush Inamdar <inamdara@uci.edu>
2024-01-24 21:44:38 -0800

    4th commit of HW2.

---
+++ src/coins_transfer.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include "coins.hpp"
+#include "coins.cpp"
+
+
+int main() {
+	Coins piggyBank(50, 50, 50, 50);
+	Coins pocket(8, 3, 6, 8);
+	int chips_cost = 149;
+
+	cout << "Initial piggy bank: " << piggyBank << endl;
+	cout << "Initial pocket: " << pocket << endl;
+
+	
+	Coins needed_coins_for_chips = coins_required_for_cents(chips_cost);
+	cout << "Bought chips using pocket" << endl;
+	cout << "Chips required the coins: " << needed_coins_for_chips << endl;
+	pocket.extract_exact_change(needed_coins_for_chips);
+	cout << "Pocket after paying for Chips: " << pocket << endl;
+
+	Coins needed_coins_for_transfer = coins_required_for_cents(405);
+	cout << "Transferring money to pocket" << endl;
+	cout << "Coins for transfer: : " << needed_coins_for_transfer << endl;
+	piggyBank.extract_exact_change(needed_coins_for_transfer);
+	pocket.deposit_coins(needed_coins_for_transfer);
+	cout << "Pocket: " << pocket << endl;
+	cout << "Piggy Bank: " << piggyBank << endl;
+
+	cout << "Piggy Bank Total: " << print_cents(total_value_in_cents(piggyBank), cout) << endl;
+	
+	
+
