93cd5b6278408d7aba7035ca7284efa9d42ae6e3
Aarush Inamdar <inamdara@uci.edu>
2024-02-05 12:23:48 -0800

    7th

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -60,22 +60,101 @@ TEST(StringFunction, strcmp) {
 	String::strcpy(man, "foa");
 	int value = String::strcmp(result, man); 
 	EXPECT_EQ(value, 14);
-
-TEST(StringFunction, strlen) {
-    EXPECT_EQ(String::strlen(""), 0);
-    EXPECT_EQ(String::strlen("foo"), 3);
 }
 
-TEST(StringFunction, strcpy) {
+
+TEST(StringFunction, strcmp) {
+	char result[10];
+    char man[10];
+	String::strcpy(result, "foo");
+	
+	String::strcpy(man, "foa");
+	int value = String::strcmp(result, man); 
+	EXPECT_EQ(value, 14);
+
+	String::strcpy(man, "foo");
+	value = String::strcmp(result, man); 
+	EXPECT_EQ(value, 0);
+
+	String::strcpy(man, "foy");
+	value = String::strcmp(result, man); 
+	EXPECT_EQ(value, -10);
+}
+
+TEST(StringFunction, strncmp) {
     char result[10];
-    EXPECT_EQ(String::strcpy(result, "foo"), result);
-    EXPECT_STREQ(result, "foo");
+    char man[10];
+	String::strncpy(result, "foo", MAXLEN);
+	
+	String::strncpy(man, "foa", MAXLEN);
+	int value = String::strncmp(result, man, 4); 
+	EXPECT_EQ(value, 14);
 
-    EXPECT_EQ(String::strcpy(result, "a"), result);
-    EXPECT_STREQ(result, "a");
+	value = String::strncmp(result, man, 2); 
+	EXPECT_EQ(value, 0);
 
-    EXPECT_EQ(String::strcpy(result, ""), result);
-    EXPECT_STREQ(result, "");
+	String::strncpy(man, "foy", MAXLEN);
+	value = String::strncmp(result, man, 4); 
+	EXPECT_EQ(value, -10);
+
+
+	char test1[10] = "test";
+	char right[10] = "testa";
+
+	value = String::strncmp(test1, right, 5); 
+	EXPECT_LT(value, 0);
+
 }
+
+TEST(StringFunction, reverse_cpy) {
+    char result[10];
+    char man[10];
+
+
+	String::strcpy(result, "foo");
+	String::reverse_cpy(man,result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
