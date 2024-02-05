1fabf90822df12056c5baf215284fc9a50b03eb2
Aarush Inamdar <inamdara@uci.edu>
2024-02-05 12:21:39 -0800

    6th

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -11,7 +11,6 @@ TEST(StringFunction, strdup) {
     EXPECT_TRUE(false);
 }
 
-
 TEST(StringFunction, strlen) {
     EXPECT_EQ(String::strlen(""), 0);
     EXPECT_EQ(String::strlen("foo"), 3);
@@ -30,33 +29,53 @@ TEST(StringFunction, strcpy) {
 }
 
 TEST(StringFunction, strncpy) {
-    EXPECT_TRUE(false);
+    char result[MAXLEN];
+	EXPECT_STREQ(String::strncpy(result, "foo", MAXLEN), "foo");
+	EXPECT_STREQ(String::strncpy(result, "olaf", MAXLEN), "olaf");
+
 }
 
 TEST(StringFunction, strcat) {
-    EXPECT_TRUE(false);
+    char src[MAXLEN] = "World";
+	char dest[MAXLEN] = "Hello";
+
+	EXPECT_STREQ(String::strcat(dest, src), "HelloWorld");
 }
 
 TEST(StringFunction, strncat) {
-    EXPECT_TRUE(false);
+    char src[MAXLEN] = "World";
+	char dest[MAXLEN] = "Hello";
+
+	EXPECT_STREQ(String::strncat(dest, src, MAXLEN), "HelloWorld");
+
+	EXPECT_STREQ(String::strncat(dest, src, 3), "HelloWorldWor");
+
 }
 
 TEST(StringFunction, strcmp) {
-    EXPECT_TRUE(false);
+	char result[10];
+    char man[10];
+	String::strcpy(result, "foo");
+	
+	String::strcpy(man, "foa");
+	int value = String::strcmp(result, man); 
+	EXPECT_EQ(value, 14);
+
+TEST(StringFunction, strlen) {
+    EXPECT_EQ(String::strlen(""), 0);
+    EXPECT_EQ(String::strlen("foo"), 3);
 }
 
-TEST(StringFunction, strncmp) {
-    EXPECT_TRUE(false);
+TEST(StringFunction, strcpy) {
+    char result[10];
+    EXPECT_EQ(String::strcpy(result, "foo"), result);
+    EXPECT_STREQ(result, "foo");
+
+    EXPECT_EQ(String::strcpy(result, "a"), result);
+    EXPECT_STREQ(result, "a");
+
+    EXPECT_EQ(String::strcpy(result, ""), result);
+    EXPECT_STREQ(result, "");
 }
 
-TEST(StringFunction, reverse_cpy) {
-    EXPECT_TRUE(false);
-}
 
-TEST(StringFunction, strchr) {
-    EXPECT_TRUE(false);
-}
-
-TEST(StringFunction, strstr) {
-    EXPECT_TRUE(false);
-}
