8dcd35638cf9329afcdde7e41eb6ed99a1441099
Aarush Inamdar <inamdara@uci.edu>
2024-02-12 12:51:12 -0800

    1st

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -8,20 +8,30 @@ using list::Node;
 TEST(ListTests, FromStringBasic) {
     Node* const foo_list_head = list::from_string("foo");
     Node* foo_list = foo_list_head;
-
+	
     EXPECT_EQ(foo_list->data, 'f');
-    // ASSERT instead of EXPECT means: end the test here if this fails, do not try to continue
+    
+	// ASSERT instead of EXPECT means: end the test here if this fails, do not try to continue
     // running. This is useful to prevent early crashes.
-    ASSERT_NE(foo_list->next, nullptr);
-
+    
+	ASSERT_NE(foo_list->next, nullptr);
+	
     foo_list = foo_list->next;
     EXPECT_EQ(foo_list->data, 'o');
     ASSERT_NE(foo_list->next, nullptr);
-
+	
     foo_list = foo_list->next;
     EXPECT_EQ(foo_list->data, 'o');
-    ASSERT_EQ(foo_list->next, nullptr);
+    
+	/*ASSERT_EQ(foo_list->next, nullptr);
 
+
+	Node* const emptyStringHead = list::from_string("");
+	Node* eString = emptyStringHead;
+
+	EXPECT_EQ(eString->data, nullptr);*/
+	
+	
     list::free(foo_list_head);
 }
 
@@ -29,11 +39,174 @@ TEST(ListTests, Length) {
     Node* const head = list::from_string("foo");
     EXPECT_EQ(list::length(head), 3);
     list::free(head);
+	
+	Node* const head2 = list::from_string("hello");
+	EXPECT_EQ(list::length(head2), 5);
+	list::free(head2);
+
+
+	Node* const head3 = list::from_string("");
+    EXPECT_EQ(list::length(head3), 0);
+    list::free(head3);
+
+
 }
+
+
+TEST(ListTests, Compare) {
+    EXPECT_EQ(list::compare(list::from_string("hello"), list::from_string("hello")), 0);
+
+	EXPECT_EQ(list::compare(list::from_string("hella"), list::from_string("hellb")), -1);
+
+	EXPECT_EQ(list::compare(list::from_string("helld"), list::from_string("hellb")), 2);
+
+	EXPECT_EQ(list::compare(list::from_string("hella"), list::from_string("hellb"), 4), 0);
+
+	EXPECT_EQ(list::compare(list::from_string("heatd"), list::from_string("heftd"), 3), -5);
+
+
+
+}
+
+
+TEST(ListTests, Copy) {
+    Node* const head = list::from_string("foo");
+	Node* myNode = list::copy(head);
+	
+	std::cout << "head: " << std::endl;
+	list::print(std::cout, head);
+	std::cout<< "\nmyNode: "  << std::endl;
+	list::print(std::cout, myNode);
+	
+	EXPECT_EQ(list::compare(head, myNode), 0);
+
+
+	
+	Node* const extreme  = list::from_string("");
+	Node* myExtreme = list::copy(extreme);
+	EXPECT_EQ(list::compare(extreme, myExtreme), 0);
+
+}
+
+
+
+
+TEST(ListTests, Append) {
+    
+	Node* hwa = list::append(list::from_string("hello"), list::from_string("world"));
+	Node* hw = list::from_string("helloworld");
+	std::cout << "Hwa: " << std::endl;
+	list::print(std::cout, hwa);
+	std::cout << "Hw: helloworld" << std::endl;
+
+
+	EXPECT_EQ(list::compare(hwa, hw), 0);
+
+
+
+	Node* hwb = list::append(list::from_string("hi"), list::from_string("world"));
+	Node* hwo = list::from_string("helloworld");
+
+	EXPECT_NE(list::compare(hwb, hwo), 0);
+
+}
+
+TEST(ListTests, Last) {
+    Node* hw = list::from_string("helloworld");
+	EXPECT_EQ(list::compare(last(hw), list::from_string("d")),0);
+	std::cout << "Function Last: " << std::endl;
+	
+	list::print(std::cout, last(hw));
+
+
+
+	Node* nothing = list::from_string("a");
+	EXPECT_EQ(list::compare(last(nothing), nothing), 0);
+
+
+	Node* hwZ = list::from_string("helloworldZ");
+	EXPECT_NE(list::compare(last(hwZ), list::from_string("Zd")),0);
+
+}
+
+
+
+
+TEST(ListTests, Nth) {
+    Node* myNode = list::from_string("HelloWorld");
+	
+	Node* myP = list::nth(myNode, 3);
+	EXPECT_EQ(myP->data, 'l');
+
+
+	myP = list::nth(myNode, 6);
+	EXPECT_EQ(myP->data, 'W');
+
+
+	myP = list::nth(myNode, 1);
+	EXPECT_EQ(myP->data, 'H');
+
+}
+
+TEST(ListTests, Index) {
+    Node* longNode = list::from_string("HelloWorld");
+	Node* shortNode = list::from_string("World");
+
+	EXPECT_EQ(index(longNode, shortNode), 5);
+
+	shortNode = list::from_string("d");
+	EXPECT_EQ(index(longNode, shortNode), 9);
+
+	shortNode = list::from_string("el");
+	EXPECT_EQ(index(longNode, shortNode), 1);
+}
+
+
+TEST(ListTests, FindChar) {
+    Node* longNode = list::from_string("HelloWorld");
+	
+	char myChar = 'W';
+	EXPECT_EQ(list::compare(list::find_char(longNode, myChar), list::from_string("World")), 0);
+
+	myChar = 'r';
+	EXPECT_EQ(list::compare(list::find_char(longNode, myChar), list::from_string("rld")), 0);
+	EXPECT_NE(list::compare(list::find_char(longNode, myChar), list::from_string("World")), 0);
+
+	myChar = 'z';
+	EXPECT_EQ(list::find_char(longNode, myChar), nullptr);
+
+
+}
+
+TEST(ListTests, FindList) {
+    Node* longNode = list::from_string("HelloWorld");
+	
+	Node* shortNode = list::from_string("oWor");
+	EXPECT_EQ(list::compare(list::find_list(longNode, shortNode), list::from_string("oWorld")), 0);
+
+	shortNode = list::from_string("rld");
+	EXPECT_EQ(list::compare(list::find_list(longNode, shortNode), list::from_string("rld")), 0);
+	EXPECT_NE(list::compare(list::find_list(longNode, shortNode), list::from_string("World")), 0);
+
+	shortNode = list::from_string("z");
+	EXPECT_EQ(list::find_list(longNode, shortNode), nullptr);
+
+}
+
+
+
+/*
+
+TEST(ListTests, ReplaceMe) {
+    EXPECT_TRUE(false);
+}
+
+
+
 
 // Add remaining tests below. All tests should follow
 // the format of `TEST(ListTests, <TestName>){}`.
 
 TEST(ListTests, ReplaceMe) {
     EXPECT_TRUE(false);
-}
+}*/
---
+++ src/list.cpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include "list.hpp"
+//#include "string.hpp"
+
+
+namespace list{
+
+Node* from_string(const char* s) {
+	if (!s || s[0]=='\0') {
+		return nullptr;
+	}
+
+	Node* head = new Node{.data=*s};	
+	Node* p = head;
+	
+	while (*s!='\0') {
+		++s;
+		p->next = new Node{.data=*s};
+		p = p->next;	
+		}
+	p = nullptr;	
+	return head;
+}
+
+// deletes all nodes of linked list
+void free(Node* head){
+	while (head!=nullptr) {
+		Node* nextNode = head->next;
+		delete head;
+		head = nextNode;
+	}
+}
+
+// prints linked list to ostream out
+void print(std::ostream& out, Node* head) {
+	for (Node* p=head; p!= nullptr; p=p->next) {
+		out << p->data << ' ';
+	}
+}
+// returns an exact duplicate of linked list starting at head
+Node* copy(Node* head) {
+	if (head!=nullptr) {
+		Node* newHead = new Node{.data=head->data};
+		Node* myPointer = newHead;	
+		for (Node* p=head->next; p!=nullptr; p = p->next) {
+			newHead->next = new Node{.data=p->data, .next=nullptr};
+			newHead=newHead->next;
+		}
+		return myPointer;
+	} else {
+		return nullptr;
+	}
+	
+}	
+		
+
+// similar to strcmp but for lists
+int compare(Node* lhs, Node* rhs) {
+	Node* lp = lhs;
+	Node* rp = rhs;
+
+	while (lp!=nullptr && rp!=nullptr) {
+		if (lp->data != rp->data) {
+			return lp->data-rp->data;
+		}
+		lp = lp->next;
+		rp = rp->next;
+	}
+	if (lp==nullptr && rp==nullptr) {
+		return 0;
+	} else if (lp == nullptr) {
+		return 'a'-rp->data-1;
+	} else {
+		return lp->data-'a'+1;
+	}
+}
+
+// similar to strncmp but for lists
+int compare(Node* lhs, Node* rhs, int n) {
+	Node* lp = lhs;
+	Node* rp = rhs;
+	
+	while (lp!=nullptr && rp!=nullptr && n!=0) {
+		if (lp->data != rp->data) {
+			return lp->data-rp->data;
+		}
+		lp = lp->next;
+		rp = rp->next;
+		n--;
+	}
+	return 0 ;
+}
+
+// counts number of nodes in linked list
+int length(Node* head) {
+	if (head==nullptr) return 0;
+	
+	int res = 0;
+	for (Node*p = head; p!=nullptr; p=p->next) {
+		++res;
+	}
+	return res-1;
+}
+
+
+// returns reversed copy of linked list
+Node* reverse(Node* head) {
+
+}
+
+
+
+// returns new list containing all nodes of lhs followed by all nodes of rhs
+Node* append(Node* lhs, Node* rhs) {
+	Node* lc = copy(lhs);
+	Node* rc = copy(rhs);
+	
+	last(lc)->next = rc;
+
+	return lc;
+}
+
+// returns index of node in list starting at head
+int index(Node* head, Node* node){
+
+	Node *p = head;
+	int i = 0;
+	while (compare(p, node, 1) != 0 && p!=nullptr) {
+		p= p->next;
+		i++;
+	}
+	return i;
+}
+
+// similar to strchr but for a linked list
+Node* find_char(Node* head, char c) {
+	Node *p = head;
+
+	while (p!=nullptr) {
+		if (p->data != c){
+			p = p->next;
+		} else {
+		return p;
+		}
+	}
+	return p;
+}
+
+
+// similar to strstr but for two linked lists
+Node* find_list(Node* haystack, Node* needle) {
+	Node* pH = haystack;
+	Node* pN = needle;
+
+	int needleLen = length(needle);
+
+	while (pH!=nullptr) {
+		if (compare(pH, pN, needleLen) == 0) {
+			return pH;
+		}
+		pH = pH->next;
+	}
+	return nullptr;
+
+
+}
+
+
+
+
+// get the nth node of linked list
+Node* nth(Node* head, int n){
+	Node* p = head;
+	int i = 1;
+	while (i!=n && p!=nullptr) {
+		p = p->next;
+		i++;
+	}
+	return p;
+}
+
+// get the last node of linked list
+Node* last(Node* head){
+	if (head==nullptr) return nullptr;
+	
+
+	Node* rabbit = head;
+	
+	while (rabbit->next->next != nullptr) {
+		rabbit = rabbit->next;
+	}
+	return rabbit;
+}
+
+}
+
