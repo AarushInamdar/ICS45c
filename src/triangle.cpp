9202d52c8a1fba503a238465496508d2d3ce068c
Aarush Inamdar <inamdara@uci.edu>
2024-02-21 21:30:49 -0800

    redone triangle draw

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -10,17 +10,29 @@ double Triangle::area() const  {
 }
 
 void Triangle::draw(std::ostream& out) const {
-int offset = std::max(height,base);
+	/*int offset = std::max(height,base);
 
-for (int i =0; i<height; i+=2) {
-	for (int j=0; j<(offset-i)/2; ++j) {
-		out << ' ';
+	for (int i =0; i<height; i+=2) {
+		for (int j=0; j<(offset-i)/2; ++j) {
+			out << ' ';
+		}
+		for (int k=0; k<=i; ++k) {
+			out << '*';
+		}
+		out << '\n';
+	}*/
+
+	int offset = std::max(height, base);
+
+	for (int i = 0; i < height; i += 2) {
+		for (int j = 0; j < (offset - i) / 2; ++j) {
+			out << ' ';
+		}
+		for (int k = 0; k <= i; ++k) {
+			out << '*';
+		}
+		out << '\n';
 	}
-	for (int k=0; k<=i; ++k) {
-		out << '*';
-	}
-	out << '\n';
-}
 }
 
 Triangle* Triangle::clone() const {
